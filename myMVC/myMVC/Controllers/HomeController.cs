a98ffbd80e2df07c7d112a50ce2621908c6a4ecf
Administrator100 <sachin100kaushik@outlook.com>
2017-02-07 00:26:10 -0800

    add project

---
+++ myMVC/myMVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public string Index(string id,string name)
+        {
+            return "Id=" + id+"Name="+name;
+        }
+
+        public String GetMvcVersion()
+        {
+            return typeof(Controller).Assembly.GetName().Version.ToString();
+        }
+
+        public string GetMethod()
+        {
+            return "Getmethod call";
+        }
+
+    }
+}
